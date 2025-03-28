921ac8b67e670f1042970662930deb0df4de1500
Killian-Aidalinfo <killian.stein@aidalinfo.fr>
2025-03-28 20:26:03 +0100

    add pnpm build and other agent gpt 4o and 4o mini

--- src/mastra/agents/index.ts
+++ src/mastra/agents/index.ts
@@ -2,17 +2,28 @@ import { openai } from '@ai-sdk/openai';
 import { Agent } from '@mastra/core/agent';
 import { PGVECTOR_PROMPT } from "@mastra/rag";
 import { vectorQueryTool } from "../helpers/vectorManagement";
-
 import { Memory } from "@mastra/memory";
- 
-const memory = new Memory();
-export const weatherAgent = new Agent({
-  name: 'RAG',
-  instructions: `
+
+const prompt = `
 Process queries using the provided context. Structure responses to be concise and relevant.
 ${PGVECTOR_PROMPT}
-`,
+`
+const
+
+memory = new Memory();
+
+export const grandOralO4Mini = new Agent({
+  name: 'grand-oral-o4-mini',
+  instructions: prompt,
   model: openai('gpt-4o-mini'),
+  tools: { vectorQueryTool },
+  memory: memory,
+});
+
+export const grandOralO4 = new Agent({
+  name: 'grand-oral-o4',
+  instructions: prompt,
+  model: openai('gpt-4o'),
   tools: { vectorQueryTool },
   memory: memory,
 });
--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,14 +1,14 @@
 
 import { Mastra } from '@mastra/core/mastra';
 import { createLogger } from '@mastra/core/logger';
-import { weatherAgent } from './agents';
+import { grandOralO4Mini, grandOralO4 } from './agents';
 import {initVector} from './helpers/vectorManagement';
 import { PgVector } from '@mastra/pg';
 
 
 const pgVector = new PgVector(process.env.POSTGRES_CONNECTION_STRING!);
 export const mastra = new Mastra({
-  agents: { weatherAgent },
+  agents: { grandOralO4Mini, grandOralO4 },
   vectors: { pgVector },
   logger: createLogger({
     name: 'Mastra',
