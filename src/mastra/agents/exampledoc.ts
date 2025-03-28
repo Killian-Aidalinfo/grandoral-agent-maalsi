3868d91b1e9334af5b99b0a0ae25e6a05ad47370
Killian-Aidalinfo <killian.stein@aidalinfo.fr>
2025-03-28 16:09:12 +0100

    WIP: work on vector loading

--- src/mastra/agents/exampledoc.ts
+++ src/mastra/agents/exampledoc.ts
@@ -1,81 +1,10 @@
 import { embedMany, embed } from "ai";
 import { openai } from "@ai-sdk/openai";
-import { PgVector } from "@mastra/pg";
 import { MDocument, rerank } from "@mastra/rag";
-import { z } from "zod";
 import { createVectorQueryTool } from "@mastra/rag";
 import { mastra } from "../../mastra";
 import { readdir, readFile } from 'fs/promises';
 import { join, extname, basename } from 'path';
-// 1. Initialize document
-const doc = MDocument.fromMarkdown(`
- # La consommation de l'IT et le BYOD
-
-## Contexte
-
-**Définition​**
-
-- La consumérisation désigne le mélange des utilisations personnelle et professionnelle des technologies, particulièrement chez les jeunes employés mobiles.​
-
-**Exemple** ​
-
-- Usage de WhatsApp pour des communications professionnelles, malgré son design pour usage privé.​
-
-**Impact sur les services informatiques​**
-
-- Les services doivent gérer des technologies personnelles tout en sécurisant les réseaux professionnels.​
-
-**Flux inversé d’innovation​**
-
-- Les innovations grand public s'intègrent dans les entreprises après avoir pénétré les foyers.
-
-**LE BYOD​**
-
-Le BYOD (Bring Your Own Device) est l'une des manifestations les plus évidentes de la consumérisation de l’informatique. ​
-​
-En permettant aux employés d’utiliser leurs appareils personnels (smartphones, tablettes, ordinateurs) pour accéder aux systèmes d’entreprise, le BYOD matérialise ce mélange des sphères personnelle et professionnelle
-
-**Etude du BYOD**
-
-![BYOD Etude](./assets/BYODetude.png)
-
-Référence : [BYOD Etude](https://www.grandviewresearch.com/industry-analysis/bring-your-own-device-market)
-
-2ème étude :
-
-![BYOD Etude 2ème étude](./assets/Etude2Byod.png)
-
-Référence : [BYOD Etude 2ème étude](https://www.mordorintelligence.com/fr/industry-reports/byod-market
-)
-
-
-## Enjeux
-
-**Enjeu Technologique**
-
-- Comment gérer la diversité des appareils personnels tout en garantissant la sécurité et la compatibilité ?​
-
-**Enjeu Ecologique :​**
-
-- Le BYOD permet-il de réduire l’impact environnemental des équipements numériques en entreprise ?​
-
-**Enjeu Légal​**
-
-- Quels sont les risques juridiques liés à la confidentialité et à la protection des données personnelles dans un cadre BYOD ?​
-
-
-
-## Références
-
-Context :
-
-https://www.lemagit.fr/definition/Consumerisation-de-linformatique#:~:text=La%20consum%C3%A9risation%20de%20l'informatique,la%20consum%C3%A9risation%20de%20l'informatique.
-
-
-https://www.marketsandmarkets.com/Market-Reports/enterprise-mobility-334.html   
-    
-`);
-
 
 /**
  * Génère un identifiant unique à partir du chemin du fichier.
@@ -116,10 +45,18 @@ async function collectMarkdownFiles(directory, exclude = []) {
 
 
 export async function initVector() {
-  const markdownFiles = await collectMarkdownFiles(directory, exclude);
-
+  const rootDirectory = join('../../src/content/fiche-de-chris');
+  const markdownFiles = await collectMarkdownFiles(rootDirectory, []);
+  const vectorStore = mastra.getVector('pgVector');
+  const indexName = 'myCollection';
+  try {
+    await vectorStore.deleteIndex({ indexName });
+  } catch (error) {
+    console.warn(`Index '${indexName}' could not be deleted or did not exist:`, error);
+  }
   for (const filePath of markdownFiles) {
     const content = await readFile(filePath, 'utf8');
+    console.log("content !!", content);
     const doc = MDocument.fromMarkdown(content)
     // 2. Create chunks
     const chunks = await doc.chunk({
@@ -134,7 +71,6 @@ export async function initVector() {
       model: openai.embedding("text-embedding-3-small"),
     });
 
-    const vectorStore = mastra.getVector('pgVector');
 
     await vectorStore.createIndex({
       indexName: 'myCollection',
@@ -145,37 +81,10 @@ export async function initVector() {
       vectors: embeddings,
       metadata: chunks.map(chunk => ({
         text: chunk.text,
-        source: 'transformer-paper'
+        source: 'fiche-de-chris'
       })),
     });
   }
-  // const query = 'explain boyd';
-
-  // // Get query embedding
-  // const { embedding: queryEmbedding } = await embed({
-  //   value: query,
-  //   model: openai.embedding('text-embedding-3-small'),
-  // });
-
-  // // Get initial results
-  // const initialResults = await vectorStore.query({
-  //   indexName: 'myCollection',
-  //   queryVector: queryEmbedding,
-  //   topK: 3,
-  // });
-
-  // // Re-rank results
-  // const rerankedResults = await rerank(initialResults, query, openai('gpt-4o-mini'), {
-  //   weights: {
-  //     semantic: 0.5,  // How well the content matches the query semantically
-  //     vector: 0.3,    // Original vector similarity score
-  //     position: 0.2   // Preserves original result ordering
-  //   },
-  //   topK: 3,
-  // });
-  // console.log("Initial results !!",initialResults);
-  // console.log("Rerank !!",rerankedResults);
-
 }
 
 export const vectorQueryTool = createVectorQueryTool({
--- src/mastra/agents/index.ts
+++ src/mastra/agents/index.ts
@@ -2,6 +2,10 @@ import { openai } from '@ai-sdk/openai';
 import { Agent } from '@mastra/core/agent';
 import { PGVECTOR_PROMPT } from "@mastra/rag";
 import { vectorQueryTool } from "./exampledoc";
+
+import { Memory } from "@mastra/memory";
+ 
+const memory = new Memory();
 export const weatherAgent = new Agent({
   name: 'RAG',
   instructions: `
@@ -10,4 +14,5 @@ ${PGVECTOR_PROMPT}
 `,
   model: openai('gpt-4o-mini'),
   tools: { vectorQueryTool },
+  memory: memory,
 });
